553cbab2f103f88416e94dc829d2e9c6e2b17e4a
Olha S <ozka1984@ukr.net>
2019-12-05 17:25:20 +0200

    5_12_2

---
+++ seventh/src/main/java/seventh/com/Animal.java
@@ -0,0 +1,27 @@
+package seventh.com;
+
+enum Type {Dog, Cat, Bird, Elephant, Caw};
+
+public class Animal {
+    private String name;
+    private int age;
+    private Type  animalType;
+
+    Animal (Type type, int age, String name){
+        this.name = name;
+        this.age = age;
+        this.animalType = type;
+    }
+
+    int getAge(){
+        return age;
+    }
+
+    String getName(){
+        return name;
+    }
+
+    Type getType(){
+        return animalType;
+    }
+}
--- seventh/src/main/java/seventh/com/Seventh.java
+++ seventh/src/main/java/seventh/com/Seventh.java
@@ -28,13 +28,27 @@ public class Seventh{
 
 //7.2
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 @FunctionalInterface
-interface ISeventh{
-    methodSeven (int [] array);
+interface ISeventh <T> {
+    T [] methodSeven (T [] array);
 }
 
 public class Seventh{
     public static void main(String[] args) {
+        List<Animal> animals = new ArrayList(new Animal(Type.Bird, 3, "Flee"),
+                new Animal(Type.Cat, 4, "Murka"),
+                new Animal(Type.Dog, 2, "Black")
+        );
 
+        List <Animal> listAnimals = animals.stream().
+                filter(a -> ((a.getAge() <= 5) && (a.getName().startsWith("A") || a.getName().startsWith("B") || a.getName().startsWith("C")))).
+                sorted().
+                limit(5).
+                collect(Collectors.toList());
     }
 }
